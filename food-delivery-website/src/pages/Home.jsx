d3420c8c1600e7a5cd05d768cbff6ac963267ca9
Aishwarya Kesharwani <anku.kesharwani17@gmail.com>
2025-11-22 12:31:28 +0530

    food

---
+++ food-delivery-website/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <>
+      {" "}
+      <div className="bg-slate-200 w-full min-h-screen">
+        <Nav />
+        <div></div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
